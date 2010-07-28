13e19bf5441abc903a270eb5abfc985d75a7507b
James Halliday <substack@gmail.com>
2010-07-27 18:09:37 -0800

    leaves and negative tests to go with the example, also s/tests/test/

---
+++ test/negative.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+var Traverse = require('traverse');
+var sys = require('sys');
+
+exports['negative update test'] = function (assert) {
+    var obj = [ 5, 6, -3, [ 7, 8, -2, 1 ], { f : 10, g : -13 } ];
+    var fixed = Traverse(obj).modify(function (x) {
+        if (x < 0) this.update(x + 128);
+    }).get();
+    
+    assert.equal(
+        sys.inspect(fixed),
+        '[ 5, 6, 125, [ 7, 8, 126, 1 ], { f: 10, g: 115 } ]',
+        'Negative values += 128'
+    );
+    
+    assert.equal(
+        sys.inspect(obj),
+        '[ 5, 6, -3, [ 7, 8, -2, 1 ], { f: 10, g: -13 } ]',
+        'Original references not modified'
+    );
+}
+
