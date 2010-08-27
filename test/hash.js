249ec0fcd2b62c6701488c17083433049060ae15
James Halliday <substack@gmail.com>
2010-08-26 19:20:04 -0800

    length, clone, and copy

--- test/hash.js
+++ test/hash.js
@@ -11,7 +11,9 @@ exports['hash traversal'] = function (assert) {
     assert.equal(hash1.b, 3);
     
     var ref2 = { foo : [1,2], bar : [4,5] };
-    var hash2 = Hash.map(ref2, function (v) { v.unshift(v[0] - 1); return v });
+    var hash2 = Hash(ref2).clone.map(
+        function (v) { v.unshift(v[0] - 1); return v }
+    ).items;
     assert.equal(ref2.foo.join(' '), '1 2');
     assert.equal(ref2.bar.join(' '), '4 5');
     assert.equal(hash2.foo.join(' '), '0 1 2');
