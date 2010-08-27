d98c125d9f9f17ca08260ed57ede2b1fac26da11
James Halliday <substack@gmail.com>
2010-08-26 20:28:43 -0800

    oh right and this example file

---
+++ examples/hash.js
@@ -0,0 +1,23 @@
+var Hash = require('traverse/hash');
+var sys = require('sys');
+
+var obj = Hash({ a : 1, b : 2 })
+    .map(function (v) { return v + 1 })
+    .update({ b : 30, c : 42 })
+    .filter(function (v) { return v % 2 == 0 })
+    .tap(function () {
+        var anyC = this.some(function (value, key) { return key == 'c' });
+        // or just this.keys.some, but anyways
+        console.log(anyC
+            ? "There's a C key this far."
+            : "There's no C key this far."
+        );
+    })
+    .items
+;
+
+/*  Output:
+    There's a C key this far.
+    { a : 2, b : 30, c : 42 }
+*/
+console.log(sys.inspect(obj));
