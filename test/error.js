89fc65c5c9f2778cb63d583b1fcd83a31ca104f4
Jordan Harband <ljharb@gmail.com>
2022-10-12 22:43:47 -0700

    [eslint] fix indentation and whitespace

--- test/error.js
+++ test/error.js
@@ -2,10 +2,10 @@ var test = require('tape');
 var traverse = require('../');
 
 test('traverse an Error', function (t) {
-    var obj = new Error("test");
-    var results = traverse(obj).map(function (node) {});
-    t.same(results, { message: 'test' });
-    
-    t.end();
+	var obj = new Error("test");
+	var results = traverse(obj).map(function (node) {});
+	t.same(results, { message: 'test' });
+
+	t.end();
 });
 
