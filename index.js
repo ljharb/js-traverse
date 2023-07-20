172df3824181ac3836e4c57df1fe690f160b3c49
Patrick Ruhkopf <patrick@ruhkopf.me>
2023-07-20 18:08:42 -0400

    improve type checking of buffer objects

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function copy(src) {
 			dst = { message: src.message };
 		} else if (isBoolean(src) || isNumber(src) || isString(src)) {
 			dst = Object(src);
-		} else if (src instanceof Buffer) {
+		} else if (typeof Buffer !== 'undefined' && typeof Buffer.isBuffer === 'function' && Buffer.isBuffer(src)) {
 			dst = Buffer.alloc(src.length, src);
 		} else if (src instanceof Uint8Array || src instanceof ArrayBuffer) {
 			dst = src.slice(0);
